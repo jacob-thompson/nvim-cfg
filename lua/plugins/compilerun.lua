b2fb360218347ac0d425b3abfa8a49d39bba3827
Jacob Thompson <jacobalthompson@gmail.com>
2024-12-11 14:55:11 -0800

    add visual mode keymap for compilerun

--- lua/plugins/compilerun.lua
+++ lua/plugins/compilerun.lua
@@ -1,4 +1,5 @@
-vim.keymap.set("n", "<F5>", "<cmd>CompileRun<CR>")
-vim.keymap.set("i", "<F5>", "<Esc><cmd>CompileRun<CR>")
+vim.keymap.set("n", "<F5>", "<Cmd>CompileRun<CR>")
+vim.keymap.set("i", "<F5>", "<Esc><Cmd>CompileRun<CR>")
+vim.keymap.set("v", "<F5>", "<Esc><Cmd>CompileRun<CR>")
 
 return { "jacob-thompson/vim-compilerun", name = "vim-compilerun" }
