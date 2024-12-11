bbb67b6a4ffd27f9451e1ab43db39ef0ac346cc4
Jacob Thompson <jacobalthompson@gmail.com>
2024-12-11 14:38:24 -0800

    update compilerun keymaps

--- lua/plugins/compilerun.lua
+++ lua/plugins/compilerun.lua
@@ -1,13 +1,4 @@
-vim.keymap.set("n", "<F5>", vim.cmd.CompileRun)
-
-vim.keymap.set("i", "<F5>", function ()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-c>', true, false, true), 'n', true)
-    vim.cmd.CompileRun()
-end)
-
-vim.keymap.set("v", "<F5>", function ()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-c>', true, false, true), 'n', true)
-    vim.cmd.CompileRun()
-end)
+vim.keymap.set("n", "<F5>", "<cmd>CompileRun<CR>")
+vim.keymap.set("i", "<F5>", "<Esc><cmd>CompileRun<CR>")
 
 return { "jacob-thompson/vim-compilerun", name = "vim-compilerun" }
