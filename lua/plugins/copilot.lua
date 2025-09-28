408a3b43c9919d679f9987923e1454cef3a7a2be
Jacob Thompson <jacobalthompson@gmail.com>
2025-09-28 11:44:34 -0700

    remap completion accept to shift-tab

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,1 +1,13 @@
+-- helper function for hotkey mapping
+function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+vim.g.copilot_no_tab_map = true
+map("i", "<S-Tab>", "copilot#Accept('<CR>')", { silent = true, expr = true })
+
 return { "github/copilot.vim", name = "copilot" }
